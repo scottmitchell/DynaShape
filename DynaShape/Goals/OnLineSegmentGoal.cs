d52975519e0258d85350fb8daae09a84c82ba7da
Scott Mitchell <scott.mitchell@autodesk.com>
2018-11-15 22:27:25 -0800

    Added OnLineSegment goal.

---
+++ DynaShape/Goals/OnLineSegmentGoal.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Autodesk.DesignScript.Geometry;
+using Autodesk.DesignScript.Runtime;
+
+
+namespace DynaShape.Goals
+{
+    [IsVisibleInDynamoLibrary(false)]
+    public class OnLineSegmentGoal : Goal
+    {
+        public Triple TargetLineStart;
+        public Triple TargetLineEnd;
+        public Triple TargetSegmentVector;
+
+
+        public OnLineSegmentGoal(List<Triple> nodeStartingPositions, Triple lineStart, Triple lineEnd, float weight = 1f)
+        {
+            TargetLineStart = lineStart;
+            TargetLineEnd = lineEnd;
+            TargetSegmentVector = TargetLineEnd - TargetLineStart;
+            Weight = weight;
+            StartingPositions = nodeStartingPositions.ToArray();
+            Moves = new Triple[StartingPositions.Length];
+            Weights = new float[StartingPositions.Length];
+        }
+
+
+        public OnLineSegmentGoal(List<Triple> nodeStartingPositions, Line line, float weight = 1f)
+            : this(
+                  nodeStartingPositions,
+                  line.StartPoint.ToTriple(),
+                  line.EndPoint.ToTriple(),
+                  weight)
+        {
+        }
+
+
+        public override void Compute(List<Node> allNodes)
+        {
+            for (int i = 0; i < NodeCount; i++)
+            {
+                var position = allNodes[NodeIndices[i]].Position;
+                Triple v = TargetLineEnd - TargetLineStart;
+                Triple u = TargetLineStart - position;
+                var t = -1 * (v.Dot(u) / v.Dot(v));
+
+                Triple q;
+                if (t < 0)
+                {
+                    q = TargetLineStart - position;
+                }
+                else if (t >= 0 && t <= 1)
+                {
+                    var temp = (1 - t) * TargetLineStart + t * TargetLineEnd;
+                    q = temp - position;
+                }
+                else
+                {
+                    q = TargetLineEnd - position;
+                }
+
+                Moves[i] = q;
+                Weights[i] = Weight;
+            }
+        }
+    }
+}
